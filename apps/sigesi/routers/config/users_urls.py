51aeff2fbd9ee08e5c2f718881020ffcb58fe19f
Julian Villamizar <julianvillamizardev@gmail.com>
2026-04-11 23:23:34 -0500

    user directories fixed

--- apps/sigesi/routers/config/users_urls.py
+++ apps/sigesi/routers/config/users_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.sigesi.views.users import create_user
+from apps.sigesi.views.config.user_view import create_user
 
 urlpatterns = [
     path('users/', create_user, name='create_user'),
--- apps/sigesi/views/config/user_view.py
+++ apps/sigesi/views/config/user_view.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from apps.sigesi.serializers import UserCreateSerializer
+from apps.sigesi.serializers.config.user_serializer import UserCreateSerializer
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
